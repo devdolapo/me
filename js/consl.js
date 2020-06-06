7a1d8e9561a5d9aba977c3f718e10eacc4d8e720
Toriiaa <59958826+Toriiaa@users.noreply.github.com>
2020-06-06 13:10:23 +0100

    improvements

---
+++ js/consl.js
@@ -0,0 +1,13 @@
+// const change = document.getElementById("change");
+// change = document.replaceChild(h3, h4);
+
+// console.log(change);
+// var tag = document.getElementsByTagName("h3");
+// console.log(tag);
+// window.screen.width = function () {
+//   if (document.body.screen < 800 || document.documentElement.screen < 800) {
+//     document.getElementsById("change").replaceChild(h4, h3);
+//   } else {
+//     document.getElementsById("change").replaceChild(h3, h4);
+//   }
+// };
