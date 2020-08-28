aa62df5c8e6006c446439bb49c54cc62191908c5
Toriiaa <59958826+Toriiaa@users.noreply.github.com>
2020-08-28 16:49:37 +0100

    new commits

--- js/consl.js
+++ js/consl.js
@@ -1,13 +1,12 @@
-// const change = document.getElementById("change");
-// change = document.replaceChild(h3, h4);
+ document.querySelector("#button").addEventListener('submit', function(e){
+    Hello();
 
-// console.log(change);
-// var tag = document.getElementsByTagName("h3");
-// console.log(tag);
-// window.screen.width = function () {
-//   if (document.body.screen < 800 || document.documentElement.screen < 800) {
-//     document.getElementsById("change").replaceChild(h4, h3);
-//   } else {
-//     document.getElementsById("change").replaceChild(h3, h4);
-//   }
-// };
+
+    e.preventDefault(),
+});
+Hello(){
+    alert:{"hi this is a string"};
+}
+
+
+
